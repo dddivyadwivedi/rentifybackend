061bf82612ea4523913a23c91409c817d4a12597
dddivyadwivedi <dddivya7980@gmail.com>
2024-05-28 01:13:22 +0530

    Fixed database connection issue

--- src/config/dataSource.config.ts
+++ src/config/dataSource.config.ts
@@ -6,6 +6,7 @@ const host = process.env.DB_HOST;
 const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306;
 const username = process.env.DB_USER;
 const database = process.env.DB_NAME;
+const password = process.env.DB_PASSWORD
 const sync: boolean = process.env.DB_SYNC && process.env.DB_SYNC === 'true';
 
 const AppDataSource = new DataSource({
@@ -13,6 +14,7 @@ const AppDataSource = new DataSource({
   host: host,
   port: port,
   username: username,
+  password,
   database: database,
   entities: [__dirname + '/../../dist/entity/**/*.entity{.js,.ts}'],
   synchronize: sync,
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,6 +6,7 @@ const host = process.env.DB_HOST;
 const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306;
 const username = process.env.DB_USER;
 const database = process.env.DB_NAME;
+const password = process.env.DB_PASSWORD
 const sync: boolean = process.env.DB_SYNC && process.env.DB_SYNC === 'true';
 
 const ORMConfig: TypeOrmModuleOptions = {
@@ -13,6 +14,7 @@ const ORMConfig: TypeOrmModuleOptions = {
   host,
   port,
   username,
+  password,
   database,
   entities: [__dirname + '/../../dist/entity/**/*.entity{.js,.ts}'],
   synchronize: sync,
